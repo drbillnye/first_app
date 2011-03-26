2d5ffe93eac308e374f28e95265a0bbe08783462
root <root@vmnye.(none)>
2011-03-26 08:13:55 -0400

    -Initial bangerang

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '419e6c0dc7afe5ba242c84fe31af171262b72b187ebc00db009eae9c51cef8fd21e6cc5953763037a13895bc69882922c40e9ed980eed58d408ad8a741334fe5'
